da880db0c2e81990c7581c68669be6b56c617c86
Stephane <son.trinh.dev@gmail.com>
2017-12-29 02:30:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,7 +37,7 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  return array.pop()
+  array.pop()
 }
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1)
