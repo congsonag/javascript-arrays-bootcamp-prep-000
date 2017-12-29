9311eb7ca5ef84a636db0a31f174572f0af749a2
Stephane <son.trinh.dev@gmail.com>
2017-12-29 02:02:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,15 +1,6 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-//arrays chocolateBars is an array containing
-
-function addElementToBeginningOfArray(array, element) {
-  return [element, ...array]
-}
-
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  array = array.unshift(element)
-  return array
-}
-
-function addElementToEndOfArray(array, element) {
-  return [...array, element]
-}
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+]
